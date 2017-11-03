492ce5acc8a2f6e3b64772f5cbebb797a47a7754
Andrew Jarrett <ahrjarrett@gmail.com>
2017-11-03 00:29:39 -0600

    refactor filter out of redux state to be managed by react router

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,20 +1,37 @@
-import { connect } from 'react-redux'
-import { setVisibilityFilter } from '../actions'
-import Link from './Link'
+import React from 'react'
+import { Link } from 'react-router'
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter,
-})
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick() {
-    dispatch(setVisibilityFilter(ownProps.filter))
-  },
-})
-
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link)
+const FilterLink = ({ filter, children }) => (
+  <Link
+    to={filter === 'all' ? '' : filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black'
+    }}
+  >
+    {children}
+  </Link>
+)
 
 export default FilterLink
+
+// import { connect } from 'react-redux'
+// import { setVisibilityFilter } from '../actions'
+// import Link from './Link'
+//
+// const mapStateToProps = (state, ownProps) => ({
+//   active: ownProps.filter === state.visibilityFilter,
+// })
+//
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+//   onClick() {
+//     dispatch(setVisibilityFilter(ownProps.filter))
+//   },
+// })
+//
+// const FilterLink = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Link)
+//
+// export default FilterLink
