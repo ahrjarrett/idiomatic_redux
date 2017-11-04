7d126128043f125b66d67a9daf22fd836f11be0b
Andrew Jarrett <ahrjarrett@gmail.com>
2017-11-04 10:49:26 -0600

    broke addLoggingToDispatch

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ todos app based on Dan Abramov’s idiomatic redux refactoring
 
 bm 11
 
+11/04/17: addLogginToDispatch to store fn currently isn't working
+
--- src/configureStore.js
+++ src/configureStore.js
@@ -3,12 +3,30 @@ import todoApp from './reducers'
 import throttle from 'lodash/throttle'
 import { loadState, saveState } from './localStorage'
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  if(!console.group) return rawDispatch
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c previous state', 'color:gray', store.getState())
+    console.log('%c action', 'color:blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color:green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
 /* exporting configureStore separately is helpful
  * because we can create as many instances of the store
  * for purposes of testing. */
 const configureStore = () => {
   const persistedState = loadState()
   const store = createStore(todoApp, persistedState)
+
+  if(process.env.NODE_ENV !== 'production')
+    addLoggingToDispatch(store)
 
   /* when store changes, save state to localStorage;
    * in order not to pass UI state (visibility filter),
